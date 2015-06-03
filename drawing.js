3d5030976a7d8ac0d5160aca0caf69801d71b42b
miguellopezbecona <miguelopez314@gmail.com>
2015-06-03 14:24:47 +0200

    Weight labels will rotate if they are inverted

--- drawing.js
+++ drawing.js
@@ -205,6 +205,18 @@ function tick() {
     edgelabels.attr('dx',function(d,i) { return getLinkHalfLength(d.source.id, d.target.id); })
 
     edgepaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y; return path});
+
+    // Prevents weight labels being inverted
+    edgelabels.attr('transform',function(d,i){
+        if (d.target.x<d.source.x){
+            bbox = this.getBBox();
+            rx = bbox.x+bbox.width/2;
+            ry = bbox.y+bbox.height/2;
+            return 'rotate(180 '+rx+' '+ry+')';
+        }
+        else
+            return 'rotate(0)';
+    });
 }
 
 function getLinkHalfLength(source, target){
